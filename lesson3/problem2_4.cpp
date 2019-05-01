9336c83dfe13b190ec5bfa5fb403f36e4c98f175
eric <eric.kim25@stu.siskorea.org>
2019-05-01 20:53:52 +0900

    Correction

--- lesson3/problem2_4.cpp
+++ lesson3/problem2_4.cpp
@@ -3,20 +3,12 @@ using namespace std;
 
 int main()
 {
-
-
-
-
-
   int N, i, sum = 0;
   cin >> N;
 
   for( i = 1; i < N+1; i++)
     sum += i;
-    //std::cout << ++i + ++i + N << '\n';
   cout << sum << endl;
-
-
-
+  
   return 0;
 }
