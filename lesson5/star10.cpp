5ff951c4e871adaf72c003ac04a194e917641465
eric <eric.kim25@stu.siskorea.org>
2019-05-15 12:04:28 +0900

    star10.cpp

---
+++ lesson5/star10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   int N, i, j, k, l, m;
+
+   cin >> N;
+   for (i = 0; i < N; i++)
+   {
+     for (k = 1; k < 2; k++) {
+       std::cout << "**" << '\n';
+     }
+     for (j = 0; j < 2; j++) {
+       std::cout << "**" << '\n';
+     }
+   }
+    return 0;
+ }
