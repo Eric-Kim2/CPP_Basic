61f4b261c42fda0d0726e8b605a13015f07a536b
eric <eric.kim25@stu.siskorea.org>
2019-06-01 11:33:15 +0900

    Q1

---
+++ lesson7/Q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+void print(int number)
+{
+  std::cout << number << '\n';
+}
+
+
+
+
+int main(float number)
+{
+  int a;
+  cin >> a;
+
+  print(a);
+  return 0;
+}
